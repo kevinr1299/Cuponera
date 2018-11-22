d58f8ced1c16c640f0c64bc947128bf056a0e705
Kevin Romero <a1299r5@gmail.com>
2018-11-21 23:30:43 -0600

    Canje

--- src/main/java/sv/edu/udb/www/Cuponera/Rest/CuponRest.java
+++ src/main/java/sv/edu/udb/www/Cuponera/Rest/CuponRest.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import sv.edu.udb.www.Cuponera.Entities.Cliente;
 import sv.edu.udb.www.Cuponera.Entities.Cupon;
-import sv.edu.udb.www.Cuponera.Entities.Usuario;
 import sv.edu.udb.www.Cuponera.Repository.ClienteRepository;
 import sv.edu.udb.www.Cuponera.Repository.CuponRepository;
 import sv.edu.udb.www.Cuponera.Repository.EstadoCuponRepository;
@@ -53,21 +50,21 @@ public class CuponRest {
 						cupon.setEstadoCupon(estadoRepository.getOne(2));
 						cupon.setFechaCanje(new Date());
 						cuponRepository.save(cupon);
-						return ResponseEntity.ok().body("Cupon canjeado");
+						return ResponseEntity.ok("Cupón canjeado");
 					}else {
-						return ResponseEntity.status(400).body("El DUI del propietario del cupon y el ingresado no coinciden");
+						return ResponseEntity.status(400).body("El DUI del propietario del cupón y el ingresado no coinciden");
 					}
 				}else if(cupon.getEstadoCupon().getId() == 2){
-					return ResponseEntity.status(400).body("El cupon ya esta canjeado");
+					return ResponseEntity.status(400).body("El cupón ya esta canjeado");
 				}else {
-					return ResponseEntity.status(400).body("El cupon ya esta canjeado");
+					return ResponseEntity.status(400).body("El cupón ya esta vencido");
 				}
 			}else {
-				return ResponseEntity.status(404).body("No se encontro ningun cupon");
+				return ResponseEntity.status(404).body("No se encontro ningun cupón con ese número");
 			}
 		}catch(Exception ex) {
 			System.out.println(ex);
-			return ResponseEntity.status(500).body("No se pudo canjear el cupon");
+			return ResponseEntity.status(500).body("No se pudo canjear el cupón");
 		}
 	}
 	
